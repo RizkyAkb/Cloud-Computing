bfa4f176a2ff6b6467864ab15c836f0eb9d64517
Rizky Akbar Wahyu Putranto <89757057+RizkyAkb@users.noreply.github.com>
2024-11-22 21:52:30 +0700

    First Commit

---
+++ src/main/java/cc/k12345/barang/BarangApplication.java
@@ -0,0 +1,13 @@
+package cc.k12345.barang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/k12345/barang/BarangApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.k12345.barang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
